e76771176fd60c39b6a4067d2ab243086805dd3e
lubson <l.hruban@gmail.com>
2011-12-02 14:39:43 +0100

    Initial commit with bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toodoo::Application.config.secret_token = 'c8f9b08269af2aa62c86e9686090829c18c0cd044f0e0a3c9fb925afc5f7070b41d41a4c5e2e1c1e127f435d8ede4980134cbecc5bc562521f3369209d3c76c7'
