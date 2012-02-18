d261f643f97725b169bd1ff31c39d382e0cc301b
Marek Takac <takacmarek1221@gmail.com>
2012-02-18 03:39:25 +0100

    Created Unit tests for notes and projects controllers

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper'
 
-describe NotesController do
-  let(:note) do
-    note = Factory(:note)
-    note
+describe ProjectsController do
+  let(:project) do
+    project = Factory(:project)
+    project
   end
 
   context "unregistered / unlogged users" do
 
     {
       'index'  => 'get',
+      'show'   => 'get',
+      'new'    => 'get',
       'create' => 'post',
       'edit'   => 'get',
-      'update'   => 'put',
-      'destroy' => 'delete'
+      'save'   => 'put',
+      'delete' => 'delete'
     }.each do |action, method|
       it "cannot access the #{action} action" do
-        send(method, action.dup, id: note.id)
+        send(method, action.dup, id: project.id)
         response.should redirect_to(root_path)
         flash[:alert].should eql('You must be logged in before proceeding.')
       end
---
+++ spec/factories/project_factory.rb
@@ -0,0 +1,4 @@
+Factory.define :project do |project|
+  project.name 'Toodoo'
+  project.note 'Best solution for task managment'
+end
