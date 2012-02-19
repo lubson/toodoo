982dcbe6ec1b8cef6e816b478d52f3b339bd95ae
lubson <l.hruban@gmail.com>
2012-02-19 22:34:07 +0100

    Callbacks edited

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,11 @@ class Project < ActiveRecord::Base
   belongs_to :user
   has_many   :tasks
 
-  after_initialize do |project|
-    self.status = 'active'
+  after_initialize do
+    self.status = 'active' if self.status.empty?
   end
 
   scope :default, where(status: 'default').first
   scope :recent, order('deadline asc')
-  scope :by_status, -> status do
-    where(status: status)
-  end
+  scope :by_status, -> status { where(status: status) }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,10 @@
 class Task < ActiveRecord::Base
   belongs_to :project
 
-  after_initialize do |active|
-    self.status = 'active'
+  after_initialize do
+    self.status = 'active' if self.status.empty?
   end
 
   scope :recent, order('due asc')
-  scope :by_status, -> status do
-    where(status: status)
-  end
+  scope :by_status, -> status do { where(status: status) }
 end
