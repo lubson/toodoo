18d5035778f99116ca36134d8c8a82e8149732e4
lubson <l.hruban@gmail.com>
2012-02-21 00:46:27 +0100

    List of completed tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,18 @@
 module TasksHelper
+
+  def checked_input(task)
+     if task.status == 'completed'
+      tag(:input, type: 'checkbox', checked: 'checked')
+    else
+      tag(:input, type: 'checkbox')
+    end
+  end
+
+  def checked_paragraph(task)
+    if task.status == 'completed'
+      content_tag(:p, task.name, class: 'checked')
+    else
+      content_tag(:p, task.name)
+    end
+  end
 end
